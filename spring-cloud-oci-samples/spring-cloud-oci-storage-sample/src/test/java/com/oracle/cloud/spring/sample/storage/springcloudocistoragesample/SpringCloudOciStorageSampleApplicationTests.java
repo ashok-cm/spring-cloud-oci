c37bd3f08eecf06fe02b546982f43de23c0dce0a
jitkukum <jitendra.z.kumar@oracle.com>
2023-07-26 10:36:52 +0530

    use base class in storage integration tests

--- spring-cloud-oci-samples/spring-cloud-oci-storage-sample/src/test/java/com/oracle/cloud/spring/sample/storage/springcloudocistoragesample/SpringCloudOciStorageSampleApplicationTests.java
+++ spring-cloud-oci-samples/spring-cloud-oci-storage-sample/src/test/java/com/oracle/cloud/spring/sample/storage/springcloudocistoragesample/SpringCloudOciStorageSampleApplicationTests.java
@@ -5,39 +5,24 @@
 
 package com.oracle.cloud.spring.sample.storage.springcloudocistoragesample;
 
-import com.oracle.cloud.spring.sample.common.util.FileUtils;
+import com.oracle.cloud.spring.sample.common.base.SpringCloudSampleApplicationTestBase;
 import com.oracle.cloud.spring.storage.Storage;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import java.io.File;
 import java.io.IOException;
 
 @SpringBootTest
 @EnabledIfSystemProperty(named = "it.storage", matches = "true")
 @TestPropertySource(locations="classpath:application-test.properties")
-class SpringCloudOciStorageSampleApplicationTests {
-
-    public static final String PRIVATE_KEY_PATH = "privateKey";
-    public static final String PRIVATE_KEY_CONTENT = "privateKeyContent";
+class SpringCloudOciStorageSampleApplicationTests extends SpringCloudSampleApplicationTestBase {
     public static final String TEST_BUCKET = "bucketName";
 
-    public static final String privateKeyFilePath = System.getProperty("user.home") + File.separator + "privatekey.pem";
-    public static final String privateKeyContent = System.getProperty(PRIVATE_KEY_CONTENT) != null ? System.getProperty(PRIVATE_KEY_CONTENT) :
-            System.getenv().get(PRIVATE_KEY_CONTENT);
     public static final String testBucket = System.getProperty(TEST_BUCKET) != null ? System.getProperty(TEST_BUCKET) :
             System.getenv().get(TEST_BUCKET);
-
-    @BeforeAll
-    static void beforeAll() throws Exception {
-        System.setProperty(PRIVATE_KEY_PATH, privateKeyFilePath);
-        FileUtils.createFile(privateKeyFilePath, privateKeyContent.replace("\\n", "\n"));
-    }
 
     @Autowired
     Storage storage;
@@ -46,11 +31,6 @@ class SpringCloudOciStorageSampleApplicationTests {
     void testFileUpload() throws IOException {
         ActivityInfo ainfo = new ActivityInfo("Hello from Storage integration test");
         storage.store(testBucket, ainfo.getFileName(), ainfo);
-    }
-
-    @AfterAll
-    static void AfterAll() {
-        FileUtils.deleteFile(privateKeyFilePath);
     }
 
     private class ActivityInfo {
